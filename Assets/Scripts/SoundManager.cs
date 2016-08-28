e5c39011f6aa0ec718633e7ef162bb6dd53d1524
Kalev Roomann-Kurrik <kalevrk@gmail.com>
2016-08-28 16:40:07 -0700

    setup audio and add bgm

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundManager : MonoBehaviour {
+
+	public AudioSource sfxSource;
+	public AudioSource musicSource;
+	public static SoundManager instance = null;
+
+	void Awake ()
+	{
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+	}
+
+	public void PlayClip (AudioClip clip)
+	{
+		sfxSource.clip = clip;
+		sfxSource.Play ();
+	}
+}
